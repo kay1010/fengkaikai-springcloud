28ddd56502c1717e672015774ee56860d7ca55aa
fkk <1916702304@qq.com>
2019-11-22 22:51:50 +0800

    在customerservice服务中通过@LoadBalanced注解结合RestTemplate 实现对userservice两个服务实例的负载均衡访问

--- customer-service/src/main/java/com/example/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/example/customerservice/controller/CustomerController.java
@@ -1,6 +1,9 @@
 package com.example.customerservice.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+@Api(tags = "Customer API" ,description = "customer相关Rest API")
 @RestController
 public class CustomerController {
     Logger logger= LoggerFactory.getLogger(CustomerController.class);
@@ -17,7 +20,7 @@ public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
 
-
+    @ApiOperation("获取用户姓名")
     @GetMapping("/user/{userName}")
     private String getUser(@PathVariable("userName") String userName){
         logger.info("-------CustomerController request USER-SERVICE for getUser-----");
