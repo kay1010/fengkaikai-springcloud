855bb584e7915c1b1fa3b5e2e5f3547fdf6686b3
fkk <1916702304@qq.com>
2020-01-03 15:19:29 +0800

    Feign使用和httpclient优化

---
+++ customer-service/src/main/java/com/example/customerservice/feignService/UserService.java
@@ -0,0 +1,13 @@
+package com.example.customerservice.feignService;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "USER-SERVICE")
+public interface UserService {
+    @GetMapping("/user/{userName}")
+    String getUser(@PathVariable("userName") String name);
+}
